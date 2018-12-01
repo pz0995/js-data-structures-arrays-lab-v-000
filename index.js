cd11f7bd6be1b6b454e4837d9d54da325309574d
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-01 20:14:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop("Otis");
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift("Milo");
+}
+
+function appendDriver() {
+  let newDriver = "Ralph";
+  var newArray = drivers.concat(newDriver);
+}
+
+function prependDriver() {
+  // drivers[drivers.length - 1] = "Broom";
+  let newDriver = "Broom";
+  var newArray = drivers.concat(newDriver);
+}
+
+function removeLastDriver() {
+  var last = drivers.slice(0, 2);
+}
+
+function removeFirstDriver() {
+  var first = drivers.slice(1);
+}
