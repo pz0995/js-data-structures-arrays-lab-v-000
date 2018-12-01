0362073d07543256bff084c90c06bcd60cd19088
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-01 09:07:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop("Otis");
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift("Milo");
+}
+//
+// function appendDriver() {
+//   drivers.splice(0, 0, "Broom"
+//   const drivers2 =  = drivers.slice()
+//   // drivers[drivers.length - 1] = "Broom";
+//   // const driver = ["Broom", ...'drivers'];
+// drivers.splice
+//
+  function prependDriver() {
+     const newDrivers = [...drivers, "Arnold"];
+  }
+
+function removeLastDriver() {
+  drivers.slice(2):
+}
+
+function removeFirstDriver() {
+
+  drivers.slice(-2);
+  // drivers.slice(1, 3);
+}
